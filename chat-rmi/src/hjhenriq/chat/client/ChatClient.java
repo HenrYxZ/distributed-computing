9d90441435ccea708c8179d970dbfe197c012f6a
Hernaldo Jesus Henriquez <hjhenriq@gmail.com>
2014-11-11 18:25:26 -0300

    Client still not totally working, new menu added

--- chat-rmi/src/hjhenriq/chat/client/ChatClient.java
+++ chat-rmi/src/hjhenriq/chat/client/ChatClient.java
@@ -1,5 +1,8 @@
 package hjhenriq.chat.client;
 
+import hjhenriq.chat.model.Contacts;
+import hjhenriq.chat.model.Conversation;
+import hjhenriq.chat.model.Person;
 import hjhenriq.chat.server.ChatServerIF;
 
 import java.io.IOException;
@@ -15,6 +18,7 @@ public class ChatClient extends UnicastRemoteObject implements ChatClientIF {
 	private static final long serialVersionUID = -8214387253929191454L;
 	private ChatServerIF mServer;
 	private String mName;
+	private Contacts mContacts; 
 
 	protected ChatClient(ChatServerIF chatServer) throws RemoteException {
 		super();
@@ -36,6 +40,7 @@ public class ChatClient extends UnicastRemoteObject implements ChatClientIF {
 		System.out.println("Enter new password: ");
 		String pass = sc.nextLine();
 		sc.close();
+		this.mName = name;
 		this.mServer.register(this, name, pass);
 	}
 
@@ -46,6 +51,7 @@ public class ChatClient extends UnicastRemoteObject implements ChatClientIF {
 		System.out.println("Enter your password: ");
 		String pass = sc.nextLine();
 		sc.close();
+		this.mName = name;
 		this.mServer.authenticate(this, name, pass);
 	}
 
@@ -55,16 +61,18 @@ public class ChatClient extends UnicastRemoteObject implements ChatClientIF {
 				+ "[2] Sign in with existing user" + "[0] Exit";
 		System.out.println(menu);
 		try {
-			switch (sc.nextInt()) {
-			case 1:
-				sendNewUser();
-				break;
-			case 2:
-				authenticateNamePass();
-				break;
-			default:
-				break;
+			if (sc.hasNextInt()) {
+				switch (sc.nextInt()) {
+				case 1:
+					sendNewUser();
+					break;
+				case 2:
+					authenticateNamePass();
+					break;
+				default:
+					break;
 
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -96,25 +104,81 @@ public class ChatClient extends UnicastRemoteObject implements ChatClientIF {
 
 	public void run() {
 		Scanner sc;
-		while (true) {
-			sc = new Scanner(System.in);
-			System.out.println("Running");
-			String menu = "Enter: " + "[0] to go back"
-					+ "[1] to add a new contact" + "[2] to remove a contact"
-					+ "[3] to list contacts" + "[4] to create a conversation";
+		sc = new Scanner(System.in);
+		
+		System.out.println("Running");
+		String menu = "Enter: \n" 
+				+ "[0] to go back\n"
+				+ "[1] to add a new contact\n" 
+				+ "[2] to remove a contact\n"
+				+ "[3] to list contacts\n" 
+				+ "[4] to create a conversation";
+		System.out.println(menu);
+		if (sc.hasNextInt()) {
 			switch (sc.nextInt()) {
-			case 0:
+			case 1:
+				addContact();
+			case 2:
+				removeContact();
+			case 3:
+				listContacts();
+			case 4:
+				createConversation();
+			default:
 				sc.close();
 				return;
-			case 1:
-				//TODO Menu options
 			}
-			System.out.println(menu);
-			if (sc.nextLine().equals("exit"))
-				break;
-
 		}
+	}
+
+	//------------------ Output Printing Functions --------------------------------
+	
+	private void createConversation() {
+		Conversation conv = new Conversation();
+		printConversationMenu();
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextLine()) {
+			String line = sc.nextLine();
+		}
+		sc.close();
+		
+	}
+
+	private void listContacts() {
+		printContactMenu("list");
+	}
+
+	private void removeContact() {
+		printContactMenu("remove");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		mContacts.remove(name);
 		sc.close();
 	}
 
+	private void addContact() {
+		printContactMenu("add");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		mContacts.add(new Person(name));
+		sc.close();
+	}
+
+	//------------------------ Methods for Printing --------------------------------
+	public void printConversationMenu(){
+		String menu = "Welcome to the chat! \n"
+				+ "Enter exit to close \n"
+				+ "Enter add([name]) to add someone to the conversation \n"
+				+ "Enter remove([name]) to remove someone from the conversation";
+		System.out.println(menu);
+	}
+	
+	public void printContactMenu(String action) {
+		if (action.equals("add"))
+			System.out.println("Write the name of the new contact");
+		else if (action.equals("remove"))
+			System.out.println("Write the name of the contact to be removed");
+		else
+			System.out.println("This are your contacts \n" + mContacts.toString());
+	}
 }
--- chat-rmi/src/hjhenriq/chat/model/Contacts.java
+++ chat-rmi/src/hjhenriq/chat/model/Contacts.java
@@ -1,5 +1,6 @@
 package hjhenriq.chat.model;
 
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 
 public class Contacts {
@@ -13,5 +14,26 @@ public class Contacts {
 	}
 	public void remove(Person p) {
 		this.people.remove(p);
+	}
+	public void remove(String name) {
+		Iterator<Person> i = this.people.iterator();
+		while(i.hasNext()) {
+			Person p = i.next();
+			if (p.getName().equals(name)) {
+				this.people.remove(p);
+				break;
+			}
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String s = "";
+		Iterator<Person> it;
+		Person current = null;
+		for(it = this.people.iterator(); it.hasNext(); current = it.next()) {
+			s += current.getName() + "\n";
+		}
+		return s;
 	}
 }
